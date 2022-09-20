f5263d09bdfa7016dd2949b3cdcb8618652a9369
Mortimer Meunier <mmeunier@pcalb-mm0000.univ-jfc.fr>
2022-09-20 16:39:14 +0200

    generePermutation et permutation

--- Projet_java_DES/src/defaut/TestDes.java
+++ Projet_java_DES/src/defaut/TestDes.java
@@ -9,6 +9,45 @@ public class TestDes {
 			
 		}
 		
+		
+		
+		
+		//test de GenerePermutation
+		int[] testGenerePermutation=des.generePermutation(5);
+		for(int i=0;i<testGenerePermutation.length;i++) {
+			System.out.print(testGenerePermutation[i]);
+			System.out.print(", ");
+		}
+		System.out.println();
+		System.out.println(testGenerePermutation.length);
+
+		
+		
+		
+		//test de permutation
+		int tailleTest=5;
+		int[] bloc= new int[tailleTest];
+		for(int i=0;i<tailleTest;i++) {
+			bloc[i]=i;
+		}
+		int[] tab_permutation= {3,5,2,1,4};
+		
+		int[] testPermutation=des.permutation(tab_permutation, bloc);
+		for(int i=0;i<testPermutation.length;i++) {
+			System.out.print(testPermutation[i]);
+			System.out.print(", ");
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
