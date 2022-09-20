7e7c008a76f2e4b85dd85cff23d8de4f1fcde60e
Martin Mouysset <mmouys00@pcalb-mm0000.univ-jfc.fr>
2022-09-20 16:47:46 +0200

    bit to string et string to bit

--- Projet_java_DES/src/defaut/TestDes.java
+++ Projet_java_DES/src/defaut/TestDes.java
@@ -4,10 +4,7 @@ public class TestDes {
 	
 	public static void main(String[] args) {
 		Des des = new Des();
-		for(int i : des.stringToBits("oui")) {
-			System.out.println(i);
-			
-		}
+		System.out.println(des.bitsToString(des.stringToBits("oui é non")));
 		
 	}
 
