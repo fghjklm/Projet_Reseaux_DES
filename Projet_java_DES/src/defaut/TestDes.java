7ef6051370bddfd6c5bb291d5cb8b1cc909e6fa5
Morti <mortimer.meunier12@gmail.com>
2022-09-27 18:08:12 +0200

    junit créé

---
+++ Projet_java_DES/src/defaut/TestDes.java
@@ -0,0 +1,20 @@
+package defaut;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestDes {
+
+	@Before
+	public void setUp() throws Exception {
+		Des des=new Des();
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
