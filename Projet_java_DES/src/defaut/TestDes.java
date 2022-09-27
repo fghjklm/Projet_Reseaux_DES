21298d5cc6d83da11f10002e5645369833094e17
Morti <mortimer.meunier12@gmail.com>
2022-09-27 18:12:27 +0200

    junit créé

--- Projet_java_DES/src/defaut/TestDes.java
+++ Projet_java_DES/src/defaut/TestDes.java
@@ -13,8 +13,73 @@ public class TestDes {
 	}
 
 	@Test
-	public void test() {
+	public void testStringToBits() {
 		fail("Not yet implemented");
 	}
-
+	
+	@Test
+	public void testBitsToString() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testGenerePermutation() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testPermutation() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testInvPermutation() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testDecoupage() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testRecollage_bloc() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testDecalle_gauche() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testXor() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testGenereCle() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testFonction_S() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testFonction_F() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testCrypte() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testDecrypte() {
+		fail("Not yet implemented");
+	}
+	
 }
