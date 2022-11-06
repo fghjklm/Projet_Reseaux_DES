c0899083366739765779dfb407e615c5204e9162
Mouysset Martin <martin.mouysset@gmail.com>
2022-11-06 16:06:06 +0100

    explorateur de fichier

--- Projet_Reseaux_Des/src/Interface_graphique/FenetreDecrypter.java
+++ Projet_Reseaux_Des/src/Interface_graphique/FenetreDecrypter.java
@@ -5,15 +5,19 @@ import java.awt.GridLayout;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.io.BufferedInputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.filechooser.FileSystemView;
 
 import Des.Des;
 import Des.TripleDes;
@@ -21,31 +25,35 @@ import Des.TripleDes;
 public class FenetreDecrypter extends JFrame implements MouseListener {
 	
 	private JLabel l1, l2;
-	private JTextField jt1, jt2;
-	private JButton b;
+	private JButton b, bmessage, bclés;
+	private String message_path, clés_path;
 	public FenetreDecrypter() {
-
 		super("Decryptage d'un message");
+		this.message_path = "";
+		this.clés_path = "";
 		this.setLayout(new BorderLayout());
 		JPanel p1 = new JPanel(new GridLayout(2,1));
 		JPanel p2 = new JPanel(new GridLayout(2,1));
 		this.l1 = new JLabel("Fichier du message crypté");
 		this.l2 = new JLabel("Fichier des clés");
 
-		this.jt1 = new JTextField();
-		this.jt2 = new JTextField();
-
+		
 		this.b = new JButton("envoyer");
+		this.bmessage = new JButton("Fichier du message");
+		this.bclés = new JButton("Fichier des clés");
 		p1.add(l1);
 		p1.add(l2);
-		p2.add(jt1);
-		p2.add(jt2);
+		p2.add(bmessage);
+		p2.add(bclés);
 		this.add(p1,BorderLayout.WEST);
 		this.add(p2,BorderLayout.CENTER);
 		this.add(b,BorderLayout.SOUTH);
 		b.addMouseListener(this);
+		bmessage.addMouseListener(this);
+		bclés.addMouseListener(this);
 		this.setSize(400,250);
 		this.setVisible(true);
+		
 	}
 	
 	public int[] StringToTab(String s) {
@@ -89,82 +97,129 @@ public class FenetreDecrypter extends JFrame implements MouseListener {
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-		if (jt1.getText().equals("") == false & jt2.getText().equals("") == false){
-			String cles = "";
-			String message = "";
-			try {
-				 
+		if(e.getSource() == bmessage) {
+			JFileChooser choose = new JFileChooser(
+			        FileSystemView
+			        .getFileSystemView()
+			        .getHomeDirectory()
+			    );
 
-	            // Creates a FileInputStream
-	            FileInputStream file = new FileInputStream(jt2.getText() + ".txt");
-
-	            // Creates a BufferedInputStream
-	            BufferedInputStream input = new BufferedInputStream(file);
-
-	            // Reads first byte from file
-	            int i = input .read();
-	            while (i != -1) {
-	                cles += ((char) i);
-
-	                // Reads next byte from the file
-	                i = input.read();
-	            }
-	            file.close();
-	            input.close();
-	         // Creates a FileInputStream
-	            FileInputStream file1 = new FileInputStream(jt1.getText() + ".txt");
-
-	            // Creates a BufferedInputStream
-	            BufferedInputStream input1 = new BufferedInputStream(file1);
-
-	            // Reads first byte from file
-	            int j = input1.read();
-	            while (j != -1) {
-	                message += ((char) j);
-
-	                // Reads next byte from the file
-	                j = input1.read();
-	            }
-	            file1.close();
-	            input1.close();
-			}
-		    catch (Exception ee) {
-			   
-			   FenetreErreur fe = new FenetreErreur("Fichiers non présents");
-		       ee.getStackTrace();
-		   
-		    }
-			if (message.equals("") == false & cles.equals("") == false) {
-				 String[] dees = cles.split("a");	            	       
-			     String[] lestab1 = dees[0].split(";");	              
-			     int[] masterkey1 = StringToTab(lestab1[0]);	              
-			     ArrayList<int[]>tab_cles1 = StringToArrayTab(lestab1[1]);	           
-			     int[][][] s_tab1 = StringToTabTabTab(lestab1[2]);	             
-			     Des des1 = new Des(masterkey1, tab_cles1, s_tab1);	       
-			     String[] lestab2 = dees[1].split(";");	       
-			     int[] masterkey2 = StringToTab(lestab2[0]);	       
-			     ArrayList<int[]>tab_cles2 = StringToArrayTab(lestab2[1]);	       
-			     int[][][] s_tab2 = StringToTabTabTab(lestab2[2]);	       	       
-			     Des des2 =  new Des(masterkey2, tab_cles2, s_tab2);	            
-			     String[] lestab3 = dees[2].split(";");	       
-			     int[] masterkey3 = StringToTab(lestab3[0]);	       
-			     ArrayList<int[]>tab_cles3 = StringToArrayTab(lestab3[1]);	       
-			     int[][][] s_tab3= StringToTabTabTab(lestab3[2]);	       
-			     Des des3 =  new Des(masterkey3, tab_cles3, s_tab3);      
-			     TripleDes triple = new TripleDes(des1,des2,des3);
-		         int[] tab_crypte = this.StringToTab(message);   
-			     String message_decrypte = triple.decrypter(tab_crypte);	       
-				 this.setVisible(false);
-				 FenetreNotification f = new FenetreNotification("Le message décrypté est : " + message_decrypte);
-				
+			choose.setDialogTitle("Selectionnez un fichier message");
+		    choose.setAcceptAllFileFilterUsed(false);
+		    FileNameExtensionFilter filter = new FileNameExtensionFilter("txt", "txt");
+		    choose.addChoosableFileFilter(filter);
+		    int res = choose.showSaveDialog(null);
+			if (res == JFileChooser.APPROVE_OPTION) {
+			      File file = choose.getSelectedFile();
+			      
+			      message_path = file.getAbsolutePath();
+			      bmessage.setText(message_path);
+			    
+			  
 			}
 			
-	       
 		}
-		else {				  
-			FenetreErreur fe = new FenetreErreur("Les deux fichiers doivent être renseignés");
+		if(e.getSource() == bclés) {
+			JFileChooser choose = new JFileChooser(
+			        FileSystemView
+			        .getFileSystemView()
+			        .getHomeDirectory()
+			    );
+
+			choose.setDialogTitle("Selectionnez un fichier clés");
+		    choose.setAcceptAllFileFilterUsed(false);
+		    FileNameExtensionFilter filter = new FileNameExtensionFilter("txt", "txt");
+		    choose.addChoosableFileFilter(filter);
+		    int res = choose.showOpenDialog(null);
+			if (res == JFileChooser.APPROVE_OPTION) {
+			      File file = choose.getSelectedFile();
+			      
+			      clés_path = file.getAbsolutePath();
+			      bclés.setText(clés_path);
+			    
+			  
+			}
+			
+		}
+			
+		// TODO Auto-generated method stub
+		if (e.getSource() == b) {
+		
+			if (message_path.equals("") == false & clés_path.equals("") == false){
+				String cles = "";
+				String message = "";
+				try {
+					 
+	
+		            // Creates a FileInputStream
+		            FileInputStream file = new FileInputStream(clés_path);
+	
+		            // Creates a BufferedInputStream
+		            BufferedInputStream input = new BufferedInputStream(file);
+	
+		            // Reads first byte from file
+		            int i = input .read();
+		            while (i != -1) {
+		                cles += ((char) i);
+	
+		                // Reads next byte from the file
+		                i = input.read();
+		            }
+		            file.close();
+		            input.close();
+		         // Creates a FileInputStream
+		            FileInputStream file1 = new FileInputStream(message_path);
+	
+		            // Creates a BufferedInputStream
+		            BufferedInputStream input1 = new BufferedInputStream(file1);
+	
+		            // Reads first byte from file
+		            int j = input1.read();
+		            while (j != -1) {
+		                message += ((char) j);
+	
+		                // Reads next byte from the file
+		                j = input1.read();
+		            }
+		            file1.close();
+		            input1.close();
+				}
+			    catch (Exception ee) {
+				   
+				   FenetreErreur fe = new FenetreErreur("Fichiers non présents");
+			       ee.getStackTrace();
+			   
+			    }
+				if (message.equals("") == false & cles.equals("") == false) {
+					 String[] dees = cles.split("a");	            	       
+				     String[] lestab1 = dees[0].split(";");	              
+				     int[] masterkey1 = StringToTab(lestab1[0]);	              
+				     ArrayList<int[]>tab_cles1 = StringToArrayTab(lestab1[1]);	           
+				     int[][][] s_tab1 = StringToTabTabTab(lestab1[2]);	             
+				     Des des1 = new Des(masterkey1, tab_cles1, s_tab1);	       
+				     String[] lestab2 = dees[1].split(";");	       
+				     int[] masterkey2 = StringToTab(lestab2[0]);	       
+				     ArrayList<int[]>tab_cles2 = StringToArrayTab(lestab2[1]);	       
+				     int[][][] s_tab2 = StringToTabTabTab(lestab2[2]);	       	       
+				     Des des2 =  new Des(masterkey2, tab_cles2, s_tab2);	            
+				     String[] lestab3 = dees[2].split(";");	       
+				     int[] masterkey3 = StringToTab(lestab3[0]);	       
+				     ArrayList<int[]>tab_cles3 = StringToArrayTab(lestab3[1]);	       
+				     int[][][] s_tab3= StringToTabTabTab(lestab3[2]);	       
+				     Des des3 =  new Des(masterkey3, tab_cles3, s_tab3);      
+				     TripleDes triple = new TripleDes(des1,des2,des3);
+			         int[] tab_crypte = this.StringToTab(message);   
+				     String message_decrypte = triple.decrypter(tab_crypte);	       
+					 this.setVisible(false);
+					 FenetreNotification f = new FenetreNotification("Le message décrypté est : " + message_decrypte);
+					
+				}
+				
+		       
+			}
+			else {				  
+				FenetreErreur fe = new FenetreErreur("Les deux fichiers doivent être renseignés");
+			}
 		}
 		
 	}
